d0a15985d7bcbd00a9c09ee52f3b3133196a59e0
Alejandro Gil <alejandro115935@gmail.com>
2022-02-09 23:56:10 -0600

    Last Update

--- LAB_01/LAB_01/Models/Personas.cs
+++ LAB_01/LAB_01/Models/Personas.cs
@@ -1,6 +1,7 @@
 ﻿using Lab_1.Help;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,13 +10,13 @@ namespace LAB_01.Models
     public class Personas
     {
         public int Id { get; set; }
-
+        [MaxLength(50)]
         public string Nombre { get; set; }
-
+        [MaxLength(50)]
         public string Apellido{ get; set; }
-
+        [MaxLength(11)]
         public string Telefono { get; set; }
-
+        [MaxLength(50)]
         public string Descripcion { get; set; }
 
         public static bool Save(Personas model)
